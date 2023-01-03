eae5c5de8533bc6814ed0194dc02fd8022b148cb
pamelabgodinho <112530544+pamelabgodinho@users.noreply.github.com>
2023-01-03 17:06:37 -0300

    Commit no  app js para testar o git pull no local

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("O Paulo fez esse commit na aula sem você ver")
+console.log("alterando o app js no git hub para puxar para o meu local via pull")
